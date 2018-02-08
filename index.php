23b445463de8149ae8ba880ea3899e526b1bd3c3
Joshua <joshuaericgalvezmedina@gmail.com>
2018-02-08 09:43:32 +0100

    organizacion de carpetas

--- index.php
+++ index.php
@@ -40,9 +40,10 @@
 		<head>
 			<title>Chat – Better than Slack</title>
 			<link type="text/css" rel="stylesheet" href="style.css" />
-			<link rel="stylesheet" href="lib/emojionearea.min.css">
+			<link rel="stylesheet" href="lib/css/emojionearea.min.css">
 			<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-			<script type="text/javascript" src="lib/emojionearea.min.js"></script>
+			<script type="text/javascript" src="lib/js/emojionearea.min.js"></script>
+			<script type="text/javascript" src="lib/js/chat.js"></script>
 		</head>
 
 	<?php
@@ -76,61 +77,6 @@
 						<input name="submitmsg" type="submit"  id="submitmsg" value="Mandar" />
 					</form>
 				</div>
-
-				<script type="text/javascript">
-					$(document).ready(function(){
-						//Si el usuario quiere dejar la sesión
-						$("#exit").click(function(){
-							var exit = confirm("¿Estas seguro que te quieres pirar de aqui?");
-							if(exit==true){window.location = 'index.php?logout=true';}
-						});
-						
-						$("#submitmsg").click(function(){
-							sendMessage();
-						});
-
-						function sendMessage(){
-							var clientmsg = $('.emojionearea-editor').text();
-
-							if(clientmsg.length == 0)
-								return;
-
-							$.post("post.php", {text: clientmsg});
-
-							$('.emojionearea-editor').text('');
-						}
-						
-						function loadLog(){
-							var oldscrollHeight = $("#chatbox").attr("scrollHeight"); //La altura del scroll antes de la petición
-							oldscrollHeight -= 20;
-							
-							$.ajax({
-								url: "log.html",
-								cache: false,
-								success: function(html){
-									$("#chatbox").html(html); //Inserta el log de char en el div #chatbox
-									//Auto-scroll
-									var newscrollHeight = $("#chatbox").attr("scrollHeight");
-									newscrollHeight -= 20; //La altura del scroll después del pedido
-									if(newscrollHeight > oldscrollHeight){
-										$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll hacia el fondo del div
-									}
-								},
-							});
-						}
-
-						$("#usermsg").emojioneArea({
-							events: {
-								keyup: function(editor, event){
-									if(event.which == 13)
-										sendMessage();
-								}
-							}
-						});
-
-						setInterval (loadLog, 1000);
-					});
-				</script>
 			</body>
 	</html>
 	<?php
